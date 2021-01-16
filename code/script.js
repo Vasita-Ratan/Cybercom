da2a5d3f4adbee23c2fd5b66215356ef11d46970
vasitaratan123@gmail.com <vasitaratan123@gmail.com>
2021-01-17 01:24:08 +0530

    js file added

--- code/script.js
+++ code/script.js
@@ -153,7 +153,7 @@ console.log(x);
 
 
                                                        //PRACTICE JAVASCRIPT :-1//
-
+/*
 //Q.1)create variables:-
 	var mass,height;
 
@@ -405,7 +405,7 @@ console.log(isengineer);//using if_else statement
 
 
                                                         //PRACTICE JAVASCRIPT :-2
-
+/*
 
 //Q.1 find the average of two different teams 
 
@@ -531,4 +531,199 @@ else
 	alert('john team score ' + johnavg +  ' higher then mike and mary team');
 	console.log('john team average score is:-' + johnavg);
 }
+*/
+
+/*-----------------------------------------------------------------------------------------------------------------------*/
+													//Date:-13/01/21
+													   //practice-3
+
+
+//Ans
+
+var bill1 = 124;
+var bill2 = 48;
+var bill3 = 268;
+
+function tipcalculate()
+
+	if(retirment > 0 )
+	{
+			console.log(firstname +' retirment of :- ' + retirment + ' year');
+	}
+	else
+	{
+		console.log(firstname +' already retirmented' );
+	}
+}
+yearRetirement(1999,'ratan');
+yearRetirement(1943,'manan');
+yearRetirement(1996,'karan');
+
+
+/*------------------------------------------------------------------------------------------------------------------------------*/
+													//Date:-15/01/21
+													//Practice
+
+
+//function constructor:-
+/*
+var john = {
+	name:'john',//define the variable value
+	yearofbirth:1999,
+	job:'Engineer'
+};
+
+var person = function(name,yearofbirth,job)//create function construcor parameters objects
+{
+	this.name=name;//this operator use to refer the value of this variable
+	this.yearofbirth=yearofbirth;
+	this.job=job;
+}
+//create prototype and specify one function for each objects to refer it.
+person.prototype.calculateage = function(){
+
+	console.log(2018 - this.yearofbirth);
+}
+
+person.prototype.lastname = 'bhatt';//spacify many think with prototype function
+
+var john = new person('john',1999,'Engineer');
+var jay = new person('jay',1998,'Designer');
+var mayank = new person('mayank',1997,'teacher');
+
+john.calculateage();//call single function for three objects
+jay.calculateage();
+mayank.calculateage();
+console.log(john.lastname);//call prototype function threw value;
+
+//object create benefit is allow beggist complex structure of inheritance
+
+var protofun = {
+	calculateage: function(){
+		console.log(2019 - this.calculateage);
+	}
+};
+
+var jay = object.create(protofun);//featch value directly inherite from the class function arguments constructor prototype property
+jay.name = 'jay';
+jay.yearofbirth = 1999;
+jay.job = 'doctor';
+
+var mayank = object.create(protofun,
+{
+	name = {value : 'mayank'},
+	yearofbirth = {value : 1997},
+	job = {value : 'teacher'}
+});
+
+//primitives VS objects
+
+var a = 25;
+var b = a;
+ a = 60; // this value can't affect veriable b bcz its not other copy generate same variable consiste first variable value 
+
+console.log(a);
+console.log(b);
+
+//objects
+var obj1 = {
+	name : 'Ratan',
+	age: 21
+};
+var obj2 = obj1;//change value bcz its refer same object value to reflect it.
+
+obj1.age = 21;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Function
+
+var age = 21;
+var obj = {
+	name: 'Ratan',
+	city: 'ahmedabad'
+
+};
+function change(a,b){
+	a = 22;
+	b.city = 'jamnagar';//pass the object variable value reflect out side the function 	
+} 
+change(age,obj);
+
+console.log(a);
+console.log(obj.city);
+
+//Passing function as argument
+
+var years = [1995,1996,1997,1998,1999];//create array return result
+
+function arrycalc(arr , fn)//create a call back function
+{
+     var arrayres = [];//empty array create
+
+     for(var i=0 ; i < arr.length ; i++)//create for loop for fetching array values
+     {
+     	arrayres.push(fn(arr[i]));
+     }
+     return arrayres;
+}  
+
+function calcarray(el)
+{
+	return 2016 - el;
+}
+
+function isFullage(el)
+{
+	return el >= 18;
+}
+function maxHeartage(el){
+
+	if(el >= 18 && el <= 81	)
+	{
+		return Math.round(206.9 - (0.67 * el));
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+var ages =arrycalc(years,calcarray);
+console.log(ages);
+
+var fullage =arrycalc(ages,isFullage);
+console.log(fullage);
+
+var rates = arrycalc(ages,maxHeartage);
+console.log(ages);
+console.log(rates);
+
+*/
+
+														//practice challenge(15-01-21)
+//Q.5
+
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------------------*/
+														//Date:-16/01/21
+														//Practice
+
+/*
+let array = [1,2,3,4,5,6,7,8,9]
+
+console.log('ans is :-' + array.slice(-1));
+
 */
