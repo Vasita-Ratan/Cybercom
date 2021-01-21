d453883154a71c3301ee16afed354fdfe306d8e8
vasitaratan123@gmail.com <vasitaratan123@gmail.com>
2021-01-21 13:35:10 +0530

    login.js file added

---
+++ login.js
@@ -0,0 +1,32 @@
+
+
+var Email = document.forms['form']['Email'];
+var password = document.forms['form']['password'];
+
+var Email_error = document.getElementById('Email_error');
+var password_error = document.getElementById('password _error');
+
+
+
+
+function validate() {
+	const validate = () =>{
+
+			const Emailval = Email.value.trim();
+			const passwordval = password.value.trim();
+		}
+
+	// body...
+	if(Email == vasitaratan123@gmail.com && password == 9898361031)
+	{
+		alert("login sucessfull");
+		return true;
+		widndow.location.href = "dashbord.html";
+	}
+	else if(Email == '' && password == '')
+	{
+			alert("login unsucessfull");
+	}
+}
+
+
